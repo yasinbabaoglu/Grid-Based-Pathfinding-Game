9b43e73c3de1904268984384086e3d2169c72fb2
yasinbabaoglu <yasnbabaoglu@gmail.com>
2022-12-17 17:35:46 +0300

    add

--- main.py
+++ main.py
@@ -12,14 +12,3 @@ oyun = Oyun()
 oyun.run()
 
     
-"""
-def draw_numbers(N=9,M=9):
-    offset = 35
-
-    
-    for row in disp_harita:
-        for col in row:
-            if col == "O" or col == "*":
-                n_text = font.render(str(col), True, pg.Color('green'))
-                screen.blit(n_text, pg.Vector2((col * 80) + offset + 5,(row * 80) + offset - 2))
-""" 
